fd6b04a494b4ae582ee0edeaf6adcfcb07f595d5
Jacob Jørgensen <me@jacob-j.com>
2024-08-05 11:42:21 +0200

    Moved everything to backend and added testing for pages.

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+import Header from '../components/Header.svelte';
+</script>
+
+<Header/>
+<main>
+	<h3>
+		Main page Content
+	</h3>
+	<a href="/home">Home</a>
+</main>
+
+<style>
+
+</style>
